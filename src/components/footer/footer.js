b9b8d180ca479f741c841d8c5724fca14a90436c
LittleLionMan <sibnie@web.de>
2022-07-25 04:38:38 +0200

    footer update

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,17 +1,10 @@
 import './footer.css';
-import akash from '../../ressources/Download.jpeg';
+
 
 export default function Footer() {
     return (
         <footer>
-            <ul>
-                <li>Powered by: </li>
-                <li className="symbol">
-                    <a href="https://akash.network/">
-                        <img src={akash} alt='akash' />
-                    </a>
-                </li>
-            </ul>
+            <p className='ani'>Update: React Läuft!</p>
         </footer>
     )
 }
