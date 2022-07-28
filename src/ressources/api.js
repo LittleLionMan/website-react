4d1040e03a175192bbe06202081363a68102d96d
LittleLionMan <sibnie@web.de>
2022-07-28 12:14:21 +0200

    doch nicht

---
+++ src/ressources/api.js
@@ -0,0 +1,5 @@
+export const api = {
+    weather: "013e49d9f84487d396daebe092b7aa53",
+    timezone: "9bdb139e84mshb898206982ee48bp1fa6eejsn20961e39fff0",
+    cities: "9bdb139e84mshb898206982ee48bp1fa6eejsn20961e39fff0"
+}
